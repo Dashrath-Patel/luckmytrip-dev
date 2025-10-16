9a8d19e40b648ed24b51bd06c78830cdb97a929b
ULTRAPAIN <1032210157@tcetmumbai.in>
2025-10-16 21:19:10 +0530

    Added Images , svg, Fonts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import store from "./store/store.js"
 
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
